dd68c82a8bf3089544155826fe4f25eb0980aa26
Grimaldien <43172354+Grimaldien@users.noreply.github.com>
2023-11-28 14:17:27 +0100

    Update sodmain.py
    Working on button text

--- sodmain.py
+++ sodmain.py
@@ -13,8 +13,55 @@ app.config.suppress_callback_exceptions = True
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
     html.Div([
-        html.Button(id="btn-player-stats", style={
-        'background-image': 'url(https://i.ebayimg.com/images/g/vOYAAOSwfDxfAvrn/s-l400.jpg)',
+        html.Button(
+        id="btn-player-stats",
+        children=[
+            html.Img(
+                src='https://pm1.aminoapps.com/7111/3e8ceb6dc8e354ca60a138fe71dcf09ceaa2318ar1-600-600v2_uhq.jpg',
+                style={
+                    'width': '100%',
+                    'height': '100%',
+                    'border-radius': '50%',
+                    'object-fit': 'cover',
+                },
+            ),
+            html.Div(
+                "Your Text",
+                style={
+                    'position': 'absolute',
+                    'top': '50%',
+                    'left': '50%',
+                    'transform': 'translate(-50%, -50%)',
+                    'color': 'white',
+                    'font-size': '14px',
+                    'text-align': 'center',
+                },
+            ),
+        ],
+        style={
+            'border': 'none',
+            'padding': '0',
+            'cursor': 'pointer',
+            'outline': 'none',
+            'align-items': 'center',
+            'justify-content': 'center',
+            'display': 'flex',
+            'position': 'relative',
+            'height': '75px',
+            'width': '75px',
+            'overflow': 'hidden',
+        },
+    )
+,
+        html.Button([html.Img(src='your-icon-url-here', style={'height': '20px', 'width': '20px'}), " Blackfathom Deeps Statistics"], 
+                    id="btn-bfd-stats", 
+                    style={'display': 'flex', 'align-items': 'center', 'border-radius': '50%', 'margin': '5px', 'padding': '10px 20px'}),
+        html.Button([html.Img(src='your-icon-url-here', style={'height': '20px', 'width': '20px'}), " Player versus Player"], 
+                    id="btn-pvp-stats", 
+                    style={'display': 'flex', 'align-items': 'center', 'border-radius': '50%', 'margin': '5px', 'padding': '10px 20px'}),
+        
+        html.Button(id="btn-talent-stats", style={
+        'background-image': 'url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtKzqVZvUcsh7VGVtryl8G1yTcCoRgYt5nsQ&usqp=CAU)',
         'background-size': 'cover',
         'background-position': 'center',  # Ensure the image is centered
         'border-radius': '50%',
@@ -29,19 +76,9 @@ app.layout = html.Div([
         'display': 'flex',
     }
 )
-,
-        html.Button([html.Img(src='your-icon-url-here', style={'height': '20px', 'width': '20px'}), " Blackfathom Deeps Statistics"], 
-                    id="btn-bfd-stats", 
-                    style={'display': 'flex', 'align-items': 'center', 'border-radius': '50%', 'margin': '5px', 'padding': '10px 20px'}),
-        html.Button([html.Img(src='your-icon-url-here', style={'height': '20px', 'width': '20px'}), " Player versus Player"], 
-                    id="btn-pvp-stats", 
-                    style={'display': 'flex', 'align-items': 'center', 'border-radius': '50%', 'margin': '5px', 'padding': '10px 20px'}),
-        html.Button([html.Img(src='your-icon-url-here', style={'height': '20px', 'width': '20px'}), " Talent Statistics"], 
-                    id="btn-talent-stats", 
-                    style={'display': 'flex', 'align-items': 'center', 'border-radius': '50%', 'margin': '5px', 'padding': '10px 20px'})
     ], style={'display': 'flex', 'justify-content': 'center', 'background-color': '#000000'}),  # Set the button container background to black
     html.Div(id='page-content')
-], style={'background-color': '#000000', 'height': '100vh', 'color': 'white'})  # Set the overall app background to black
+], style={'background-color': '#000000', 'height': '100vh', 'color': 'black'})  # Set the overall app background to black
 
 # Callbacks to navigate to different pages
 @app.callback(Output('url', 'pathname'),
@@ -72,16 +109,16 @@ def navigate(n1, n2, n3, n4):
 def render_page_content(pathname):
     if pathname == '/player-statistics':
         # Define layout for Player Statistics
-        return html.Div([html.H1('Player Statistics Dashboard')], style={'backgroundColor': '#000000', 'height': '100vh', 'color': 'white'})
+        return html.Div([html.H1('Player Statistics')], style={'backgroundColor': '#000000', 'height': '100vh', 'color': 'white'})
     elif pathname == '/blackfathom-deeps-statistics':
         # Define layout for Blackfathom Deeps Statistics
-        return html.Div([html.H1('Blackfathom Deeps Statistics Dashboard')], style={'backgroundColor': '#000000', 'height': '100vh', 'color': 'white'})
+        return html.Div([html.H1('Blackfathom Deeps Statistics')], style={'backgroundColor': '#000000', 'height': '100vh', 'color': 'white'})
     elif pathname == '/pvp-statistics':
         # Define layout for Player vs Player Statistics
-        return html.Div([html.H1('PvP Statistics Dashboard')], style={'backgroundColor': '#000000', 'height': '100vh', 'color': 'white'})
+        return html.Div([html.H1('PvP Statistics')], style={'backgroundColor': '#000000', 'height': '100vh', 'color': 'white'})
     elif pathname == '/talent-statistics':
         # Define layout for Talent Statistics
-        return html.Div([html.H1('Talent Statistics Dashboard')], style={'backgroundColor': '#000000', 'height': '100vh', 'color': 'white'})
+        return html.Div([html.H1('Talent Statistics')], style={'backgroundColor': '#000000', 'height': '100vh', 'color': 'white'})
     else:
         # Default page when no buttons are clicked
         return html.Div([html.H1('Welcome to the WoW Dashboard')], style={'backgroundColor': '#000000', 'height': '100vh', 'color': 'white'})
